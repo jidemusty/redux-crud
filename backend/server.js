a6f1fe4ca7f0f1469dd3333a4d8d1f318ebae389
Mustapha Adubiagbe <jidemustapha2013@gmail.com>
2018-12-21 11:20:18 -0600

    server-side validation & record creation

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,24 @@
 import express from "express";
 import mongodb from "mongodb";
+import bodyParser from 'body-parser'
 
 const app = express();
+app.use(bodyParser.json())
 const dbUrl = "mongodb://localhost:27017";
+
+function validate(data) {
+  let errors = {};
+  if (data.title === "") errors.title = "Title cannot be empty";
+  if (data.cover === "") errors.cover = "Cover cannot be empty";
+  const isValid = Object.keys(errors).length === 0;
+
+  return { errors, isValid };
+}
 
 mongodb.MongoClient.connect(
   dbUrl,
   function(err, client) {
-    const db = client.db('crudwithredux');
+    const db = client.db("crudwithredux");
     app.get("/api/games", (req, res) => {
       db.collection("games")
         .find({})
@@ -16,13 +27,31 @@ mongodb.MongoClient.connect(
         });
     });
 
+    app.post("/api/games", (req, res) => {
+      const { errors, isValid } = validate(req.body)
+
+      if (isValid) {
+        const { title, cover } = req.body
+        db.collection('games').insert({ title, cover }, (err, result) => {
+          if (err) {
+            res.status(500).json({ errors: { global: "Something went wrong" }})
+          } else {
+            res.json({ game: result.ops[0] })
+          }
+        })
+      } else {
+        res.status(400).json({ errors })
+      }
+    });
+
     app.use((req, res) => {
       res.status(404).json({
         errors: {
-          global: "Still working on it. Please try again later when we implement it"
+          global:
+            "Still working on it. Please try again later when we implement it"
         }
-      })
-    })
+      });
+    });
 
     app.listen(8080, () => console.log("Server is running on localhost:8000"));
   }
--- src/GameForm.js
+++ src/GameForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import classnames from "classnames";
 import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
 import { saveGame } from "./actions";
 
 class GameForm extends Component {
@@ -8,7 +9,8 @@ class GameForm extends Component {
     title: "",
     cover: "",
     errors: {},
-    loading: false
+    loading: false,
+    done: false
   };
 
   handleChange = e => {
@@ -35,81 +37,81 @@ class GameForm extends Component {
     if (isValid) {
       const { title, cover } = this.state;
       this.setState({ loading: true });
-      this.props
-        .saveGame({ title, cover })
-        .then(
-          () => {},
-          err =>
-            err.response
-              .json()
-              .then(({ errors }) => this.setState({ errors, loading: false }))
-        );
+      this.props.saveGame({ title, cover }).then(
+        () => {
+          this.setState({ done: true });
+        },
+        err =>
+          err.response
+            .json()
+            .then(({ errors }) => this.setState({ errors, loading: false }))
+      );
     }
   };
 
   render() {
     const { title, cover } = this.state;
 
-    return (
-      <div>
-        <form
-          className={classnames("ui", "form", { loading: this.state.loading })}
-          onSubmit={this.handleSubmit}
+    const form = (
+      <form
+        className={classnames("ui", "form", { loading: this.state.loading })}
+        onSubmit={this.handleSubmit}
+      >
+        <h1>Add new game</h1>
+
+        {!!this.state.errors.global && (
+          <div className="ui negative message">
+            <p>{this.state.errors.global}</p>
+          </div>
+        )}
+
+        <div
+          className={classnames("field", {
+            error: !!this.state.errors.title
+          })}
         >
-          <h1>Add new game</h1>
+          <label htmlFor="title">Title</label>
+          <input
+            name="title"
+            value={title}
+            onChange={this.handleChange}
+            id="title"
+          />
+          <span>{this.state.errors.title}</span>
+        </div>
 
-          {!!this.state.errors.global && (
-            <div className="ui negative message">
-              <p>{this.state.errors.global}</p>
-            </div>
+        <div
+          className={classnames("field", {
+            error: !!this.state.errors.cover
+          })}
+        >
+          <label htmlFor="cover">Cover URL</label>
+          <input
+            name="cover"
+            value={cover}
+            onChange={this.handleChange}
+            id="cover"
+          />
+          <span>{this.state.errors.cover}</span>
+        </div>
+
+        <div className="field">
+          {this.state.cover !== "" && (
+            <img
+              src={this.state.cover}
+              alt="cover"
+              className="ui small bordered image"
+            />
           )}
+        </div>
 
-          <div
-            className={classnames("field", {
-              error: !!this.state.errors.title
-            })}
-          >
-            <label htmlFor="title">Title</label>
-            <input
-              name="title"
-              value={title}
-              onChange={this.handleChange}
-              id="title"
-            />
-            <span>{this.state.errors.title}</span>
-          </div>
-
-          <div
-            className={classnames("field", {
-              error: !!this.state.errors.cover
-            })}
-          >
-            <label htmlFor="cover">Cover URL</label>
-            <input
-              name="cover"
-              value={cover}
-              onChange={this.handleChange}
-              id="cover"
-            />
-            <span>{this.state.errors.cover}</span>
-          </div>
-
-          <div className="field">
-            {this.state.cover !== "" && (
-              <img
-                src={this.state.cover}
-                alt="cover"
-                className="ui small bordered image"
-              />
-            )}
-          </div>
-
-          <div className="field">
-            <button className="ui primary button">Save</button>
-          </div>
-        </form>
-      </div>
+        <div className="field">
+          <button className="ui primary button">Save</button>
+        </div>
+      </form>
     );
+
+    return <div>{this.state.done ? <Redirect to="/games" /> : form}</div>;
   }
 }
 
